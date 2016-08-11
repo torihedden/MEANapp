179328025021a9c92398537abb17af849f16479c
Tori Hedden <tori.hedden@gmail.com>
2016-08-10 23:33:35 -0400

    tear out some language from tutorial and use more appropriate variable names

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -12,37 +12,36 @@ angular.module('mainModule', [])
 
       Todos.get()
         .success(function(data) {
-          $scope.todos = data;
+          $scope.books = data;
           console.log(data);
         });
 
       // CREATE ==================================================================
       // when submitting the add form, send the text to the node API
-      $scope.createTodo = function() {
+      $scope.createBook = function() {
 
         // validate the formData to make sure that something is there
         // if form is empty, nothing will happen
-        // people can't just hold enter to keep adding the same to-do anymore
+        // people can't just hold enter to keep adding the same book anymore
         if (!$.isEmptyObject($scope.formData)) {
 
           // call the create function from our service (returns a promise object)
           Todos.create($scope.formData)
 
-            // if successful creation, call our get function to get all the new todos
+            // if successful creation, call our get function to get all the new books
             .success(function(data) {
               $scope.formData = {}; // clear the form so our user is ready to enter another
-              $scope.todos = data; // assign our new list of todos
+              $scope.books = data; // assign our new list of books
             });
         }
       };
 
       // DELETE ==================================================================
-      // delete a todo after checking it
-      $scope.deleteTodo = function(id) {
+      $scope.deleteBook = function(id) {
         Todos.delete(id)
-            // if successful creation, call our get function to get all the new todos
+            // if successful creation, call our get function to get all the new books
           .success(function(data) {
-              $scope.todos = data; // assign our new list of todos
+              $scope.books = data; // assign our new list of books
           });
       };
 
@@ -56,9 +55,9 @@ angular.module('mainModule', [])
           .success(function(data) {
 
           //
-            $($scope.todos).each(function(index) {
-              if ($scope.todos[index]._id === data._id) {
-                $scope.todos[index].checkedOut = data.checkedOut;
+            $($scope.books).each(function(index) {
+              if ($scope.books[index]._id === data._id) {
+                $scope.books[index].checkedOut = data.checkedOut;
               }
             });
 
